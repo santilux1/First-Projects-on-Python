7f90fe24797fab08669a9598273cf005df82cfa4
Santiago Martinez <santiagoamartinez777@gmail.com>
2026-03-13 22:36:05 -0500

    Add 30-Days-Of-Python

---
+++ 30-Days-Of-Python/Challenge Day 1/helloworld.py
@@ -0,0 +1,20 @@
+# Day 1 - 30DaysOfPython Challenge - First Exercise, Hello World!
+print (3+4) # Addition
+print (3-4) # Subtraction
+print (3*4) # Multiplication
+print (3/4) # Division
+print (3%4) # Modulus
+print (3//4) # Floor Division
+print (3**4) # Exponentiation
+print ("Santiago")
+print ('Martinez')
+print("Colombia ")
+print("I am enjoying 30 days of python")
+print(type(10)) # int
+print(type(9.8)) # float   
+print(type(3.14)) # float
+print (type(4+4j)) # complex
+print (type(['Asabeneh', 'Python', 'Finland'])) # list
+print (type('Santiago')) # str
+print (type('Martinez')) # str
+print (type('Colombia')) # str
